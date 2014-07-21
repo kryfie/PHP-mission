ab6015c11d8eaace5d7a2a5c56ce6f34f104a332
kryfie <kryfie@users.noreply.github.com>
2014-07-21 13:00:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,44 +1,46 @@
 <?php
-require 'connect.inc.php';
+require 'connect_database.php';
+require 'class.php';
 
-$query = "SELECT `members` FROM `members`";
+$query = $handler->query('SELECT `members`,`id`, `date_notification` FROM `members`');
 
-if ($query_run = mysql_query($query)) {
-
-	if (mysql_num_rows($query_run)==NULL) { //if no rows in SQL, echo out: "No results found" message
-		echo 'No results found.';
-	} else {
-
-		while ($query_row = mysql_fetch_assoc($query_run)) { //fetching data from SQL to an array
-			$members = $query_row['members'];
-			
-			echo $members.'<form action="index.php" method="POST"><input type="hidden" name="members" value="'.$members.'"><input type="submit" value="Notify"></form><br>';
-		}
+if ($query->rowCount()){
+	while($r = $query->fetch(PDO::FETCH_OBJ)) {
+		$members = $r->members;
+		$user_id = $r->id;
+		$date_notification = $r->date_notification;
+		echo $members,'<form action="index.php" method="POST"><input type="hidden" name="members" value="'.$members.'">
+		<input type="hidden" name="user_id" value="'.$user_id.'"><input type="hidden" name="date_notification" value="'.$date_notification.'">
+		<input type="submit" value="Notify"></form><br>';
 	}
+} else {
+	echo "No results.";
 }
 
 
-if (isset($_POST['members'])) {
+
+if (isset($_POST['members']) && isset($_POST['user_id']) && isset($_POST['date_notification'])) {
 	$members = $_POST['members'];
+	$user_id = $_POST['user_id'];
+	$date_notification = $_POST['date_notification'];
+	$notified = "Y";
+	$diff_dates = new Dates;
+	$diff_dates_num = $diff_dates->DateCompare($date_notification);
+	$num_of_days = new Dates;
+	$days_num = $num_of_days->num_of_days;
 
-	if (!empty($members)) {
-		$query = "SELECT `id` FROM `members` WHERE `members`='$members'";
-		if ($query_run = mysql_query($query)) {
-			$user_id = mysql_result($query_run, 0, 'id');
-			$notified = "Y";
-			$query = "SELECT `member_id` FROM `notification` WHERE `member_id` = '$user_id'";
-			$query_run = mysql_query($query);
-			if (mysql_num_rows($query_row==1)) {
-				echo "User already notified";
-				} else {
-				$query = "INSERT INTO `notification` VALUES ('', '$notified', $user_id)";
-				$query_run = mysql_query($query);
-				echo "User has been notified.";
-			}
-			
 
-		}	
-	}
+	if ($diff_dates_num >= $days_num) {
+		$notification_sql = "INSERT INTO notification (notified, member_id, date_notification) VALUES ('$notified', '$user_id', NOW())";
+		$handler->query($notification_sql);
+		$date_notification_update_sql = "UPDATE members SET date_notification=NOW() WHERE members='$members'";
+		$handler->query($date_notification_update_sql);
+		echo "SQL updated";
+		} else {
+		echo "User has been already notified on $date_notification";
+
+		}
+	
 }
 
 ?>
