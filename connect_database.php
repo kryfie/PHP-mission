abed5bbd9a48d335f0da14cbbaf4b7471d0ae8bf
kryfie <kryfie@users.noreply.github.com>
2014-07-24 14:54:18 +0200

    Update connect_database.php

--- connect_database.php
+++ connect_database.php
@@ -7,10 +7,11 @@ $password = "cbainet";
 
 
 try {
-	$handler = new PDO('mysql:host='.$host_name.';dbname='.$database, $username, $password);
+	$conn = new PDO('mysql:host='.$host_name.';dbname='.$database, $username, $password);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
 	print "Error!: " . $e->getMessage() . "<br/>";
-	die();
+	
 }
 
 
