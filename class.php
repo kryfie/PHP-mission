f1d19d10c82884f5609ff0c0221c2737201ce7d7
kryfie <kryfie@users.noreply.github.com>
2014-07-28 14:44:46 +0200

    Update class.php

--- class.php
+++ class.php
@@ -12,4 +12,25 @@ class Dates {
 	}
 }
 
+class Email {
+	public function SendEmail($user, $email) {
+		$to = $email;
+		$subject = 'This is an email.';
+		$headers = 'From: Me <kryfie@gmail.com>';
+		$headers .= "MIME-Version: 1.0\r\n";
+		$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+		$message = '<html><body>';
+		$message = '<img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQQqOQL91KvrEFkWyfPvxtyq7a2U-tEeJ7Qx8sHonSXlDC-BBc-Yg" />';
+		$message .= '<h1>Hi <b>'.$user.'</b>,</h1>'."\n\n";
+		$message .= '<h2>You are being notified.</h2>';
+		
+
+		if (mail($to, $subject, $message, $headers)) {
+						echo "Email has been sent!<br>";
+					} else {
+						echo "Email not sent :(<br>";
+					}
+	}
+}
 ?>
