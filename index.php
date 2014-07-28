91d4c2dd4d5edaf174a9b59c85a2f80043632d14
kryfie <kryfie@users.noreply.github.com>
2014-07-28 14:45:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ if ($query->rowCount()) {
 		$user_id = $r->id;
 		$date_notification = $r->date_notification;
 		echo '<input type="checkbox" name="user_id[]" value="'.$user_id.'">', $members.' ('.$date_notification.')<br>';
+		
 	}
 } else {
 	echo "No results.";
@@ -25,13 +26,14 @@ echo '<input type="submit" value="Notify"></form>';
 if (isset($_POST['user_id'])) {
 	$user_id = $_POST['user_id'];
 	foreach($user_id as $element)  {
-		$sql = "SELECT members, date_notification FROM members WHERE id = $element";
+		$sql = "SELECT members, email_address, date_notification FROM members WHERE id = $element";
 		$query = $conn->prepare($sql);
 		$query->execute(array());
 
 		if($query->rowCount()) {
 			while($r = $query->fetch(PDO::FETCH_OBJ)) {
 				$members = $r->members;
+				$email = $r->email_address;
 				$date_notification = $r->date_notification;
 				
 				$diff_dates = new Dates;
@@ -40,6 +42,9 @@ if (isset($_POST['user_id'])) {
 				$days_num = $num_of_days->num_of_days;
 
 				if ($diff_dates_num >= $days_num) {
+
+					$new_mail = new Email;
+					$new_mail->SendEmail($members, $email);
 		
 					$date_notification_update_sql = "UPDATE members SET date_notification=NOW() WHERE members= :members";
 					$query2 = $conn->prepare($date_notification_update_sql);
@@ -47,9 +52,10 @@ if (isset($_POST['user_id'])) {
 					$query2->execute(array(
 						':members' => $members
 						));
-					echo 'User <strong>'.$members.'</strong> has been notified successfully. Previous notification was sent on <b>'.$date_notification.'</b><br>';
-					} else {
-					echo 'User <strong>'.$members.'</strong> has been already notified on <strong>'.$date_notification.'</strong> Next notification will be sent in <b>'.$days_num. '</b> day(s).<br>';
+					echo 'User <strong>'.$members.'</strong> has been notified successfully.<br> Previous notification was sent on <b>'.$date_notification.'</b><br>';
+					
+				} else {
+				echo 'User <strong>'.$members.'</strong> has been already notified on <strong>'.$date_notification.'</strong><br> Next notification will be sent in <b>'.$days_num. '</b> day(s).<br>';
 
 					}
 			}
@@ -64,6 +70,8 @@ if (isset($_POST['user_id'])) {
 }else{
 	echo "Select the users you would like to notify!";
 }
+
+
 
 
 ?>
